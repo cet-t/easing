e8092aa2366c839d61810597ce21b42db561b9dc
cet-t <cets021@gmail.com>
2024-08-28 17:32:25 +0900

    first commit

---
+++ ease.py
@@ -0,0 +1,105 @@
+﻿from enum import Enum
+import easing_function as f
+
+
+class EASE(Enum, int):
+    LINEAR = 0
+    ZERO = 10
+    ONE = 11
+
+    IN_SINE = 100
+    OUT_SINE = 101
+    IN_OUT_SINE = 102
+
+    IN_QUAD = 110
+    OUT_QUAD = 111
+    IN_OUT_QUAD = 112
+
+    IN_CUBIC = 120
+    OUT_CUBIC = 121
+    IN_OUT_CUBIC = 122
+
+    IN_QUART = 130
+    OUT_QUART = 131
+    IN_OUT_QUART = 132
+
+    IN_QUINT = 140
+    OUT_QUINT = 141
+    IN_OUT_QUINT = 142
+
+    IN_SEXT = 150
+    OUT_SEXT = 151
+    IN_OUT_SEXT = 152
+
+    IN_EXPO = 0xf00051
+    OUT_EXPO = 0xf00052
+    IN_OUT_EXPO = 0xf00053
+
+    IN_CIRC = 0xf00061
+    OUT_CIRC = 0xf00062
+    IN_OUT_CIRC = 0xf00063
+
+    IN_BACK = 0xf00071
+    OUT_BACK = 0xf00072
+    IN_OUT_BACK = 0xf00073
+
+    IN_ELASTIC = 0xf00081
+    OUT_ELASTIC = 0xf00082
+    IN_OUT_ELASTIC = 0xf00083
+
+    IN_BOUNCE = 0xf00091
+    OUT_BOUNCE = 0xf00092
+    IN_OUT_BOUNCE = 0xf00093
+
+
+def easing(x: float, ease: EASE) -> float | None:
+    match ease:
+        case EASE.LINEAR: return f.linear(x)
+        case EASE.ZERO: return f.zero(x)
+        case EASE.ONE: return f.one(x)
+
+        case EASE.IN_SINE: return f.isine(x)
+        case EASE.OUT_SINE: return f.osine(x)
+        case EASE.IN_OUT_SINE: return f.iosine(x)
+
+        case EASE.IN_QUAD: return f.iquad(x)
+        case EASE.OUT_QUAD: return f.oquad(x)
+        case EASE.IN_OUT_QUAD: return f.ioquad(x)
+
+        case EASE.IN_CUBIC: return f.icubic(x)
+        case EASE.OUT_CUBIC: return f.ocubic(x)
+        case EASE.IN_OUT_CUBIC: return f.iocubic(x)
+
+        case EASE.IN_QUART: return f.iquart(x)
+        case EASE.OUT_QUART: return f.oquart(x)
+        case EASE.IN_OUT_QUART: return f.ioquart(x)
+
+        case EASE.IN_QUINT: return f.iquint(x)
+        case EASE.OUT_QUINT: return f.oquint(x)
+        case EASE.IN_OUT_QUINT: return f.ioquint(x)
+
+        case EASE.IN_SEXT: return f.isext(x)
+        case EASE.OUT_SEXT: return f.osext(x)
+        case EASE.IN_OUT_SEXT: return f.iosext(x)
+
+        case EASE.IN_EXPO: return f.iexpo(x)
+        case EASE.OUT_EXPO: return f.oexpo(x)
+        case EASE.IN_OUT_EXPO: return f.ioexpo(x)
+
+        case EASE.IN_CIRC: return f.icirc(x)
+        case EASE.OUT_CIRC: return f.ocirc(x)
+        case EASE.IN_OUT_CIRC: return f.iocirc(x)
+
+        case EASE.IN_BACK: return f.iback(x)
+        case EASE.OUT_BACK: return f.oback(x)
+        case EASE.IN_OUT_BACK: return f.ioback(x)
+
+        case EASE.IN_ELASTIC: return f.ielastic(x)
+        case EASE.OUT_ELASTIC: return f.oelastic(x)
+        case EASE.IN_OUT_ELASTIC: return f.ioelastic(x)
+
+        case EASE.OUT_BOUNCE: return f.obounce(x)
+        case EASE.IN_BOUNCE: return f.ibounce(x)
+        case EASE.IN_OUT_BOUNCE: return f.iobounce(x)
+
+        case _: return None
---
+++ easing_function.py
@@ -0,0 +1,87 @@
+﻿import numpy as np
+from typing import Final
+
+
+c1: Final = 1.70158
+c2: Final = c1*1.525
+c3: Final = c1+1
+c4: Final = (2*np.pi)/3
+c5: Final = (2*np.pi)/4.5
+
+n1: Final = 7.5625
+d1: Final = 2.75
+
+
+def linear(x: float) -> float: return x
+def zero(x: float) -> float: return x*0
+def one(x: float) -> float: return x**0
+
+
+def isine(x: float) -> float: return 1-np.cos((x*np.pi)/2)
+def osine(x: float) -> float: return np.sin((x*np.pi)/2)
+def iosine(x: float) -> float: return -(np.cos(np.pi*x)-1)/2
+
+
+def iquad(x: float) -> float: return x**2
+def oquad(x: float) -> float: return 1-(1-x)**2
+def ioquad(x: float) -> float: return -(np.cos(np.pi*x)-1)/2
+
+
+def icubic(x: float) -> float: return x**3
+def ocubic(x: float) -> float: return 1-(1-x)**3
+def iocubic(x: float) -> float: return 4*x**3 if x < 0.5 else 1-(-2*x+2)**3/2
+
+
+def iquart(x: float) -> float: return x**4
+def oquart(x: float) -> float: return 1-(1-x)**4
+def ioquart(x: float) -> float: return 8*x*4 if x < 0.5 else 1-(-2*x+2)**4/2
+
+
+def iquint(x: float) -> float: return x**5
+def oquint(x: float) -> float: return 1-(1-x)**5
+def ioquint(x: float) -> float: return 16*x*5 if x < 0.5 else 1-(-2*x+2)**5/2
+
+
+def isext(x: float) -> float: return x**6
+def osext(x: float) -> float: return 1-(1-x)**6
+def iosext(x: float) -> float: return 32*x*6 if x < 0.5 else 1-(-2*x+2)**6/2
+
+
+def iexpo(x: float) -> float: return 0 if x == 0 else 1-2**(-10*x)
+def oexpo(x: float) -> float: return 1 if x == 1 else 1-2**(-10*x)
+def ioexpo(x: float) -> float: return 0 if x == 0 else 1 if x == 1 else 2**(20*x-10)/2 if x < 0.5 else (2-2**(-20*x+10))/2
+
+
+def icirc(x: float) -> float: return 1-np.sqrt(1-x**2)
+def ocirc(x: float) -> float: return np.sqrt(1-(2*x)**2)
+def iocirc(x: float) -> float: return (1-np.sqrt(1-(2*x)**2))/2
+
+
+def iback(x: float) -> float: return c3*x**3-c1*(x-1)**2
+def oback(x: float) -> float: return 1+c3*(x-1)**3+c1*(x-1)**2
+def ioback(x: float) -> float: return ((2*x)**2*((c2+1)*2*x-c2))/2 if x < 0.5 else ((2*x-2)**2*((c2+1)*(x*2-2)+c2)+2)/2
+
+
+def ielastic(x: float) -> float: return 0 if x == 0 else 1 if x == 1 else -(2**(10*x-10))*np.sin((x*10-10.75)*c4)
+def oelastic(x: float) -> float: return 0 if x == 0 else 1 if x == 1 else 2**(-10*x)*np.sin((x*10-0.75)*c4)+1
+def ioelastic(x: float) -> float: return 0 if x == 0 else 1 if x == 1 else -(2**(20*x-10)*np.sin((20*x-11.125)*c5))/2 if x < 0.5 else (2**(-20*x+10)*np.sin((20*x-11.125)*c5))/2+1
+
+
+def ibounce(x: float) -> float: return 1-obounce(1-x)
+
+
+def obounce(x: float) -> float:
+    if x < 1/d1:
+        return n1*x**2
+    elif x < 2/d1:
+        x -= 1.5
+        return n1*(x/d1)*x+0.75
+    elif x < 2.5/d1:
+        x -= 2.25
+        return n1*(x/d1)*x+0.984375
+    else:
+        x -= 2.625
+        return n1*(x/d1)*x+0.984375
+
+
+def iobounce(x: float) -> float: return x < 0.5 if (1-obounce(1-2*x))/2 else (1+obounce(2*x-1))/2
